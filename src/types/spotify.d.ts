295c48501ac9a704b9be4dec00fb6c94b27a369e
DichterDev <david.dr.ruescher@outlook.com>
2025-06-25 12:51:50 +0200

    feat: added non functional profile

--- src/types/spotify.d.ts
+++ src/types/spotify.d.ts
@@ -7,3 +7,37 @@ export interface Response<T> {
   total: number
   items: T[]
 }
+
+interface User {
+  display_name: string
+  external_urls: ExternalUrl
+  followers: {
+    href: string,
+    total: number
+  }
+  href: string
+  id: string
+  images: Image[]
+  type: string
+  uri: string
+}
+
+export interface CurrentUser extends User {
+  country: string
+  email: string
+  explicit_content: {
+    filter_enabled: boolean
+    filter_locked: boolean
+  }
+  product: string
+}
+
+interface ExternalUrl {
+  spotify: string
+}
+
+interface Image {
+  url: string
+  height: number
+  width: number
+}
---
+++ src/views/ProfileView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { CurrentUser } from '@/types/spotify';
+import { ref } from 'vue';
+
+const user = ref<CurrentUser>()
+</script>
+
+<template>
+  <div>
+    <span>{{ user?.display_name }}</span>
+    <a :href="user?.external_urls.spotify"></a>
+    <img :src="user?.images[0].url" />
+  </div>
+</template>
+
+<style lang="css" scoped></style>
