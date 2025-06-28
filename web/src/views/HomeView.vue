495f6d0a484ca8ffca3fa5a29e5cb61abfa86dbe
DichterDev <david.dr.ruescher@outlook.com>
2025-06-28 21:50:48 +0200

    feat: basic home page styling

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,19 +1,33 @@
-<script setup lang="ts">
-import { useAuthStore } from '@/stores/auth';
-
-
-const auth = useAuthStore()
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div class="home-container">
     <RouterLink to="/editor">Editor</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
     <RouterLink to="/profile">Profile</RouterLink>
-    <RouterLink to="/login">Login</RouterLink>
     <RouterLink to="/settings">Settings</RouterLink>
   </div>
-  <div v-show="auth.isLoggedIn">
-    Logged
-  </div>
 </template>
+
+<style lang="css" scoped>
+.home-container {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  justify-content: center;
+
+  gap: 1em;
+}
+
+.home-container>* {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 20%;
+  aspect-ratio: 1 / 1;
+  border-radius: 0.25em;
+
+  background-color: var(--primary);
+}
+</style>
