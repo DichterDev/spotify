a0f061eae99fe1053de86d6b34eb219e3c1cb9ec
DichterDev <david.dr.ruescher@outlook.com>
2025-06-28 21:42:37 +0200

    feat: basic search styling

--- web/src/components/Search.vue
+++ web/src/components/Search.vue
@@ -5,8 +5,21 @@ const emit = defineEmits(["search:change", "refresh:click"])
 
 <template>
   <div class="search-container">
-    <input placeholder="Search..." @input="emit('search:change', ($event.target as HTMLInputElement).value)" />
+    <input class="search-input" placeholder="Search..."
+      @input="emit('search:change', ($event.target as HTMLInputElement).value)" />
   </div>
 </template>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+.search-container {
+  display: flex;
+  flex-direction: row;
+}
+
+.search-input {
+  flex: 1;
+  background-color: var(--secondary);
+  border-radius: 0.25em;
+  padding: 5px;
+}
+</style>
