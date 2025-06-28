cb67b4e5c3391b0967405caea07398abca78d90f
DichterDev <david.dr.ruescher@outlook.com>
2025-06-28 21:54:34 +0200

    feat: basic login page styling

--- web/src/views/LoginView.vue
+++ web/src/views/LoginView.vue
@@ -8,9 +8,29 @@ const auth = useAuthStore()
 
 <template>
   <div class="login-container">
-    <div>To access this application you need to login</div>
+    <div>Login to access this application</div>
     <div>Sorry for the inconvenience :3</div>
-    <div @click="auth.login">Login</div>
-    <div @click="auth.refresh">Refresh</div>
+    <div class="login-button" @click="auth.login">Login</div>
   </div>
 </template>
+
+<style lang="css" scoped>
+.login-container {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5em;
+}
+
+.login-button {
+  height: 3em;
+  width: 8em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--primary);
+  border-radius: 0.25em;
+}
+</style>
